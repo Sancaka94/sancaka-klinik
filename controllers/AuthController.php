0e57c3f50b8bc4f2d398d8c74a879fd4fd4ed5d5
Sancaka94 <TOKOSANCAKA@GMAIL.COM>
2025-07-06 07:31:15 +0700

    auto update from watcher

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -1,4 +1,8 @@
 <?php
+// Aktifkan error reporting untuk debugging
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // File: app/controllers/AuthController.php
 
 require_once __DIR__ . '/../config/database.php';
