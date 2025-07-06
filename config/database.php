5f2799fc4855cac9c8b060bd8088af616b054c06
Sancaka94 <TOKOSANCAKA@GMAIL.COM>
2025-07-06 07:38:22 +0700

    auto update from watcher

--- config/database.php
+++ config/database.php
@@ -1,15 +1,35 @@
 <?php
-class Database {
-    private $host = 'localhost';
-    private $user = 'sancakab_admin';
-    private $pass = 'Salafyyin***94';
-    private $dbname = 'sancakab_klinik';
+// File: config/database.php
 
-    public function connect() {
-        $conn = new mysqli($this->host, $this->user, $this->pass, $this->dbname);
-        if ($conn->connect_error) {
-            die("Koneksi gagal: " . $conn->connect_error);
+class Database {
+    // Properti untuk menyimpan detail koneksi database Anda
+    // PERBAIKAN: Menggunakan kredensial yang Anda berikan.
+    private $host = "localhost";
+    private $db_name = "sancakab_klinik";
+    private $username = "sancakab_admin";
+    private $password = "Salafyyin***94";
+
+    // Properti PUBLIK untuk menyimpan objek koneksi
+    public $conn;
+
+    /**
+     * Constructor akan otomatis dijalankan saat 'new Database()' dipanggil.
+     * Fungsinya adalah membuat koneksi dan menyimpannya ke dalam $this->conn.
+     */
+    public function __construct() {
+        $this->conn = null; // Set ke null dulu untuk awal
+
+        try {
+            // Membuat objek koneksi mysqli baru
+            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+            
+            // Mengatur set karakter untuk mencegah masalah encoding
+            $this->conn->set_charset("utf8mb4");
+
+        } catch (mysqli_sql_exception $exception) {
+            // Jika koneksi gagal, tampilkan pesan error dan hentikan script
+            // Di lingkungan produksi, sebaiknya ini dicatat ke log, bukan ditampilkan ke pengguna
+            die("Koneksi Database Gagal: " . $exception->getMessage());
         }
-        return $conn;
     }
 }
